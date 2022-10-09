e3f4cef30b8909d37a2369b3a07fd18e9c965e34
MarceldZ <94352026+Semjasa83@users.noreply.github.com>
2022-10-09 02:01:15 +0200

    legal notice and imprint done, routings done

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { ImprintComponent } from './imprint/imprint.component';
-import { LegalnoticeComponent } from './legalnotice/legalnotice.component';
+
 
 const routes: Routes = [
   {
@@ -13,7 +13,6 @@ const routes: Routes = [
   },
   { path: 'home', component: HomeComponent },
   { path: 'imprint', component: ImprintComponent },
-  { path: 'legalnotice', component: LegalnoticeComponent },
 ];
 
 @NgModule({
